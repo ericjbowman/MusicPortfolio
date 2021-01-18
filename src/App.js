2b7cb417091edcbaf8f3c6829c5238aeb0e176c5
ericjbowman <eric@lazarus.enterprises>
2021-01-18 17:35:16 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import eric from './images/Eric.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+        <div className='logo'>
+          <img src={eric} alt='Eric' />
+          <span>Eric Bowman Music</span>
+        </div>
+        <div className='links'>
+          <span>About</span>
+          <span>Portfolio</span>
+          <span>Contact</span>
+        </div>
+      </nav>
+      <header>
+        <div className="demo-reel-container">
+          <iframe className='demo-reel' src="https://www.youtube.com/embed/Tv06ja-HSIc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       </header>
+      <main>
+      <section className='portfolio'>
+          <h3>Portfolio</h3>
+      </section>
+        <section className='about'>
+          <h3>About Me</h3>
+          <p>Sentient bipedal creature</p>
+        </section>
+        <section className='contact'>
+          <h3>Contact</h3>
+        </section>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
